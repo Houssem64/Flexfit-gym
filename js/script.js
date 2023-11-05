3b9c702856662f9fe0069b8b7daca786a114e529
Houssem Mehouachi <fazeputin@yandex.com>
2023-11-05 23:46:09 +0100

    tweaks 2.0
    - added scroll to top button
    - added footer icon using fontawesome

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+/* BMI calculator*/ 
+
 document.addEventListener("DOMContentLoaded", function () {
     const calculateBtn = document.getElementById("calculate-bmi");
     calculateBtn.addEventListener("click", function () {
@@ -24,3 +27,24 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+
+/* scroll back to top */
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
