b85600d2be08126669f250b4b898c93dd465b3b4
Houssem Mehouachi <fazeputin@yandex.com>
2023-11-05 22:45:38 +0100

    Tweaks
    added BMI calculator, cleaned code and root folder

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const calculateBtn = document.getElementById("calculate-bmi");
+    calculateBtn.addEventListener("click", function () {
+      const weight = parseFloat(document.getElementById("weight").value);
+      const height = parseFloat(document.getElementById("height").value) / 100; // Convert height to meters
+      const bmi = weight / (height * height);
+
+      if (!isNaN(bmi)) {
+        const bmiResult = document.getElementById("bmi-result");
+        let bmiCategory = "";
+
+        if (bmi < 18.5) {
+          bmiCategory = "Underweight";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+          bmiCategory = "Normal Weight";
+        } else if (bmi >= 25 && bmi < 29.9) {
+          bmiCategory = "Overweight";
+        } else {
+          bmiCategory = "Obese";
+        }
+
+        bmiResult.innerHTML = `<h4>Your BMI: ${bmi.toFixed(2)}</h4>
+        <p class='bmi-category'>Category: ${bmiCategory}</p>`;
+      }
+    });
+  });
